27d6f1b105b60bb6b15fb5b3d5fda4ff38aa0d33
Joses Thuo <thuokjoses@gmail.com>
2024-03-27 09:55:31 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,9 @@
+//solution
+
+const num1 = 2000
+const num2 = 100
+const divide = function(num1, num2){
+    return num1 / num2
+}
+const square = (num1) => num1**2;
+const add = (num1, num2) => num1 + num2;
